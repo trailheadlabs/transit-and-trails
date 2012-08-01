3dbb8941ce6ab8a6346af88defe5f469349e721a
jerememonteau <me@jmoe.com>
2012-08-01 10:35:40 -0700

    first commit on tnt on rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Transitandtrails::Application.config.secret_token = 'fcdcc419a6da80c2a252237ccbb29d6f99f6ab2335929c36c38c650a68bce677d5c514ad5f8e86294e55fc7a45bb2935ad195aa3f768595306525b98c0786dbd'
