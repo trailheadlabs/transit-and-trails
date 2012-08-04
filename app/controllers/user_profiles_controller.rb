27505c1f1c7024af4fd73befe8d03eea4d75b8cc
jerememonteau <me@jmoe.com>
2012-08-03 23:35:42 -0700

    quick commit 2012-08-03 23:35:42

--- app/controllers/user_profiles_controller.rb
+++ app/controllers/user_profiles_controller.rb
@@ -26,7 +26,7 @@ class UserProfilesController < ApplicationController
   # GET /user_profiles/new
   # GET /user_profiles/new.json
   def new
-    @user_profile = UserProfile.new(:user_id = current_user.id)
+    @user_profile = UserProfile.new(:user_id => current_user.id)
     respond_to do |format|
       format.html # new.html.erb
       format.json { render json: @user_profile }
