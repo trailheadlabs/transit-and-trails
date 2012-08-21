216e3b03f0e7d38bd86146a8916caabfa8d5dfd1
jerememonteau <me@jmoe.com>
2012-08-21 10:42:54 -0700

    quick commit 2012-08-21 10:42:54

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Transitandtrails::Application.routes.draw do
   namespace :api, :defaults => {:format => :json} do
     namespace :v1 do
-      resources :users
+      resources :users, :only => [:show, :index]
     end
   end
 
--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -1,6 +1,6 @@
 require "spec_helper"
 
-describe API::V1::UsersController do
+describe Api::V1::UsersController do
   render_views
 
   describe "GET index" do
