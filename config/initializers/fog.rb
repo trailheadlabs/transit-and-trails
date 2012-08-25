bd18ba384e41c5ecf546b01ae9a01fbc5a5b2f8e
jerememonteau <me@jmoe.com>
2012-08-25 10:13:44 -0700

    quick commit 2012-08-25 10:13:44

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -1,11 +1,13 @@
-S3 = Fog::Storage.new({
-  :provider                 => 'AWS',
-  :aws_access_key_id        => ENV['AWS_KEY'],
-  :aws_secret_access_key    => ENV['AWS_SECRET']
-})
+unless Rails.env.test?
+  S3 = Fog::Storage.new({
+    :provider                 => 'AWS',
+    :aws_access_key_id        => ENV['AWS_KEY'],
+    :aws_secret_access_key    => ENV['AWS_SECRET']
+  })
 
-BAOSC_S3 = Fog::Storage.new({
-  :provider                 => 'AWS',
-  :aws_access_key_id        => ENV['BAOSC_AWS_KEY'],
-  :aws_secret_access_key    => ENV['BAOSC_AWS_SECRET']
-})
+  BAOSC_S3 = Fog::Storage.new({
+    :provider                 => 'AWS',
+    :aws_access_key_id        => ENV['BAOSC_AWS_KEY'],
+    :aws_secret_access_key    => ENV['BAOSC_AWS_SECRET']
+  })
+end
