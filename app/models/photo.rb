47a31d7160e66d9bd8acdf4875fee2edf52b8a89
jerememonteau <me@jmoe.com>
2012-08-27 17:07:39 -0700

    Final API stuff and Flickr integration.

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -11,4 +11,45 @@ class Photo < ActiveRecord::Base
       Rails.logger.error e
     end
   end
+
+  # http://www.flickr.com/services/api/misc.urls.html
+  def flickr_info
+    flickr.photos.getInfo :photo_id => self.flickr_id
+  end
+
+  # eventually we should just cache all of these on the model
+  def flickr_url
+    "http://flickr.com/#{flickr.test.login.username}/#{flickr_id}"
+  end
+
+  def flickr_thumbnail_url
+    FlickRaw.url_t(flickr_info)
+  rescue
+    nil
+  end
+
+  def flickr_square_url
+    FlickRaw.url_s(flickr_info)
+  rescue
+    nil
+  end
+
+  def flickr_medium_url
+    FlickRaw.url_z(flickr_info)
+  rescue
+    nil
+  end
+
+  def flickr_original_url
+    FlickRaw.url_o(flickr_info)
+  rescue
+    nil
+  end
+
+  def flickr_large_url
+    FlickRaw.url_b(flickr_info)
+  rescue
+    nil
+  end
+
 end
--- app/views/api/v1/trailheads/photos.rabl
+++ app/views/api/v1/trailheads/photos.rabl
@@ -1,1 +1,17 @@
 collection @photos
+attributes :flickr_id => :id
+node :flickr_url do |p|
+  p.flickr_url
+end
+node :medium do |p|
+  p.flickr_medium_url
+end
+node :original do |p|
+  p.flickr_original_url || p.flickr_large_url
+end
+node :square do |p|
+  p.flickr_square_url
+end
+node :thumbnail do |p|
+  p.flickr_thumbnail_url
+end
