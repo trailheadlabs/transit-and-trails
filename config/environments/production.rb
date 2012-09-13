38363d9a952ae5f4e2cd70b7689da5d2ba6c5c9f
Jereme Monteau <me@jmoe.com>
2012-09-12 21:41:35 -0700

    quick commit 2012-09-12 21:41:34

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,7 +17,7 @@ Transitandtrails::Application.configure do
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = true
 
-  config.assets.js_compiler = :closure
+  config.assets.js_compiler = Closure::Compiler.new
 
   # Generate digests for assets URLs
   config.assets.digest = true
