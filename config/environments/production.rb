b33415de425a780b2e4e8a2018a3047c0e970d6d
Jereme Monteau <me@jmoe.com>
2012-09-17 22:51:40 -0700

    quick commit 2012-09-17 22:51:40

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,10 +12,14 @@ Transitandtrails::Application.configure do
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
-  config.assets.compress = false
+  config.assets.compress = true
+
+  config.assets.css_compressor = :yui
+
+  config.assets.js_compressor = :yui
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
