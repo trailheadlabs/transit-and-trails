1fe30b38a0b84ef92a364125b2b4b437973c129b
Jereme Monteau <me@jmoe.com>
2012-09-17 22:40:45 -0700

    quick commit 2012-09-17 22:40:45

--- Gemfile
+++ Gemfile
@@ -47,6 +47,7 @@ group :assets do
   # See https://github.com/sstephenson/execjs#readme for more supported runtimes
   gem 'therubyracer', :platforms => :ruby
   gem 'closure-compiler'
+  gem 'yui-compressor'
   gem 'uglifier', '>= 1.0.3'
 end
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,8 +14,12 @@ Transitandtrails::Application.configure do
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
+  config.assets.css_compressor = :yui
+
+  config.assets.js_compressor = :yui
+
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
