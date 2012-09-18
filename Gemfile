f8a4bf6bfab121a3822bf0ec9fdebb5de01a08e7
Jereme Monteau <me@jmoe.com>
2012-09-17 21:24:04 -0700

    quick commit 2012-09-17 21:24:03

--- Gemfile
+++ Gemfile
@@ -42,8 +42,7 @@ gem 'truncate_html'
 # in production environments by default.
 group :assets do
   gem 'sass-rails',   '~> 3.2.3'
-  gem 'coffee-rails', '~> 3.2.1'
-  gem 'closure-compiler'
+  gem 'coffee-rails', '~> 3.1'
 
   # See https://github.com/sstephenson/execjs#readme for more supported runtimes
   # gem 'therubyracer', :platforms => :ruby
--- config/application.rb
+++ config/application.rb
@@ -56,10 +56,8 @@ module Transitandtrails
     # Enable the asset pipeline
     config.assets.enabled = true
 
-    config.assets.compress = true
-
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
+    config.assets.version = '1.1'
 
     config.assets.initialize_on_precompile = false
   end
