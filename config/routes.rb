d67775d9c42969de68fd90007a7550a680b7a390
Jereme Monteau <me@jmoe.com>
2012-09-18 18:21:43 -0700

    quick commit 2012-09-18 18:21:43

--- config/routes.rb
+++ config/routes.rb
@@ -1,31 +1,33 @@
 Transitandtrails::Application.routes.draw do
-  namespace :api, :defaults => {:format => :json} do
-    namespace :v1 do
-      resources :users, :only => [:show, :index]
-      resources :attribute_categories, :only => [:show, :index]
-      resources :trailheads, :only => [:show, :index] do
-        member do
-          get 'photos'
-          get 'attributes'
-          get 'maps'
+  constraint :subdomain => /api\.rails/ do
+    namespace :api, :defaults => {:format => :json} do
+      namespace :v1 do
+        resources :users, :only => [:show, :index]
+        resources :attribute_categories, :only => [:show, :index]
+        resources :trailheads, :only => [:show, :index] do
+          member do
+            get 'photos'
+            get 'attributes'
+            get 'maps'
+          end
         end
-      end
-      resources :campgrounds, :only => [:show, :index] do
-        member do
-          get 'photos'
-          get 'attributes'
-          get 'maps'
+        resources :campgrounds, :only => [:show, :index] do
+          member do
+            get 'photos'
+            get 'attributes'
+            get 'maps'
+          end
         end
-      end
-      resources :trips, :only => [:show, :index] do
-        member do
-          get 'photos'
-          get 'attributes'
-          get 'maps'
-          get 'route'
+        resources :trips, :only => [:show, :index] do
+          member do
+            get 'photos'
+            get 'attributes'
+            get 'maps'
+            get 'route'
+          end
         end
+        resources :trailhead_attributes, :only => [:show, :index]
       end
-      resources :trailhead_attributes, :only => [:show, :index]
     end
   end
 
