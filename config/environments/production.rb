c4309ec6d27c3b9ecb89c4191c324c25d3b022c7
me@jmoe.com <me@jmoe.com>
2012-10-19 08:27:44 -0700

    tweaking asset settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,6 +24,9 @@ Transitandtrails::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+  # Compress JavaScripts and CSS
+  config.assets.compress = true
+
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
