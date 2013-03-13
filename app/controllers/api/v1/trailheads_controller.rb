ebd7a62236bfd5f832dc5c87ebe971168cc55cd9
me@jmoe.com <me@jmoe.com>
2013-03-13 13:20:57 -0700

    quick commit 2013-03-13 13:20:57

--- app/controllers/api/v1/trailheads_controller.rb
+++ app/controllers/api/v1/trailheads_controller.rb
@@ -5,7 +5,7 @@ module Api
       caches_action :index, :expires_in => 60, :cache_path => Proc.new { |c| c.params.except(:key) }
 
       def index
-        @trailheads = Trailhead.order("id").includes(:cached_park_by_bounds,:park)
+        @trailheads = Trailhead.approved.order("id").includes(:cached_park_by_bounds,:park)
         if params[:non_profit_partner_id]
           partner_id = params[:non_profit_partner_id]
           parks = Park.where(:non_profit_partner_id => partner_id)
