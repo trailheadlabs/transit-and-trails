2099213d4d6afa659430bbb5259d5d8276647e56
me@jmoe.com <me@jmoe.com>
2013-04-01 22:09:32 -0700

    quick commit 2013-04-01 22:09:32

--- app/controllers/embed/trips_controller.rb
+++ app/controllers/embed/trips_controller.rb
@@ -21,12 +21,24 @@ module Embed
       if params[:show]
         @show = params[:show].split(',')
       else
-        @show = ['description','contributor','summary','photos','map','attributes','actions','header','downloads']
+        @show = ['description','contributor','summary','photos','map','attributes','actions','header','downloads','branding']
       end
 
       if params[:hide]
         @hide = params[:hide].split(',')
         @show = @show - @hide
+      end
+
+      if params[:hide_title]
+        @hide_title = params[:hide_title] == 'true'
+      end
+
+      if params[:hide_section_labels]
+        @hide_section_labels = params[:hide_section_labels] == 'true'
+      end
+
+      if params[:full_description]
+        @full_description = params[:full_description] == 'true'
       end
 
       render "responsive_show", :layout => "embed/responsive"
