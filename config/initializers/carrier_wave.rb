bed7c255bb5fcd13c1c06db356b7288634e06cd8
me@jmoe.com <me@jmoe.com>
2013-07-23 20:01:51 -0700

    quick commit 2013-07-23 20:01:51

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,8 +1,9 @@
 if Rails.env.test? or Rails.env.cucumber?
   CarrierWave.configure do |config|
     config.storage = :file
-    config.enable_processing = false  end
-  else
+    config.enable_processing = false  
+  end
+else
   CarrierWave.configure do |config|
     config.storage = :fog
     config.fog_credentials = {
