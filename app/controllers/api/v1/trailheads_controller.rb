5be4f276ec1e92dbe011da84de269a7e83b948d6
me@jmoe.com <me@jmoe.com>
2013-10-24 11:39:50 -0700

    quick commit 2013-10-24 11:39:50

--- app/controllers/api/v1/trailheads_controller.rb
+++ app/controllers/api/v1/trailheads_controller.rb
@@ -5,7 +5,7 @@ module Api
       caches_action :index, :expires_in => 60, :cache_path => Proc.new { |c| c.params.except(:key) }
 
       def index
-        approved = (params[:approved] && params[:approved] == true) || fakse
+        approved = (params[:approved] && (params[:approved] == true)) || fakse
         @trailheads = Trailhead.where(approved:approved).order("id").includes(:cached_park_by_bounds,:park)
         if params[:non_profit_partner_id]
           partner_id = params[:non_profit_partner_id].split(",")
